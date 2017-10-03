d13b5c3f43ba7eb02eaeac4bf0f6cf1062ca27da
volcas <nnair25@gmail.com>
2017-10-03 16:48:40 +0530

    Create a file to combine the other three CSV files

---
+++ Combined.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+Views=pd.read_csv('Views.csv')
+SubCount=pd.read_csv('SubscriberCount.csv')
+VidCount=pd.read_csv('VideoCount.csv')
+
+
+
+FinalOP= pd.concat([Views['Views'], SubCount['SubCount'], VidCount['VideoCount']], axis=1)
+print(FinalOP)
+
+FinalOP.to_csv('FinalOP.csv',index=False)
